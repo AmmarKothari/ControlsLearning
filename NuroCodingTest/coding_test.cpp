5c2b8aceda701f9ddc499857a78ddb0c6d4433cb
Ammar <amarsbars@gmail.com>
2018-08-02 12:49:20 -0700

    coding test

--- NuroCodingTest/coding_test.cpp
+++ NuroCodingTest/coding_test.cpp
@@ -7,7 +7,7 @@
 
 struct Point { double x; double y; };
 struct Segment { Point start; Point end; };
-// Poinst in CCW order. Implicit edge between first and last point.
+// Points in CCW order. Implicit edge between first and last point.
 using Polygon = std::vector<Point>;
 struct Box { Point bl; Point tr; };
 
@@ -28,7 +28,7 @@ std::string BoxStr(const Box& box) {
 
 std::string PolygonStr(const Polygon& polygon) {
   std::string s = "Polygon(";
-  for (int i = 0; i < polygon.size(); ++i) {
+  for (size_t i = 0; i < polygon.size(); ++i) {
     s += PointStr(polygon[i]);
     if (i < polygon.size() - 1) {
       s += " ";
@@ -37,8 +37,13 @@ std::string PolygonStr(const Polygon& polygon) {
   return s + ")";
 }
 
+bool inside(Segment poly_edge, Segment edge) {
+
+
+}
+
 Point clipX(Point p1, Point p2, double X) {
-  double m = (p1.y - p2.y)/ ((p1.x - p2.x))
+  double m = (p1.y - p2.y)/ ((p1.x - p2.x));
   double Y = m * (X - p1.x) + p1.y;
   Point p = {X, Y};
   return p;
@@ -46,40 +51,67 @@ Point clipX(Point p1, Point p2, double X) {
 
 Polygon ClipPolygon(Polygon subj, const Box& clip) {
 
-  Polygon newsubj = subj;
-  newsubj.append(subj[0]);
+  Polygon out = subj;
+  Point BL = {clip.bl.x, clip.bl.y};
+  Point TL = {clip.bl.x, clip.tr.y};
+  Point TR = {clip.tr.x, clip.tr.y};
+  Point BR = {clip.tr.x, clip.bl.y};
+  Polygon BoxPoints = {BL, TL, TR, BR};
+
   for (int c_v=0; c_v<4; c_v++) {
+    Segment edge = {BoxPoints[c_v], BoxPoints[(c_v+1)%4]};
     
-    for (size_t p_v=0; p_v<newsubj.size()-1; p_v++) {
-      // deal with last point
-      // check inside vs outside
-      if (c_v == 0) { // left
-        if (newsubj[p_v].x < clip.bl.x) {
-          Point p;
-          p = clipX(newsubj[p_v], newsubj[p_v+1], clip.bl.x);
-        }
+    Polygon in = out;
+    out.clear();
+    Point S = in.back();
+
+    for (auto E: in) {
+      Segment poly_edge = {S, E};
+      if (inside(poly_edge, edge)) {
+    //     if (!inside(S, edge)) {
+    //       P_x = computeIntersection(S, E, edge);
+    //       out.push_back(P_x);
+    //     }
+    //     out.push_back(E);
       }
-      else if (c_v == 1) { // top
-        if (newsubj[p_v].y > clip.tr.y) {
-          newsubj[p_v].y = clip.tr.y;
-        }
-      }
-      else if (c_v == 2) { // right
-        if (newsubj[p_v].x > clip.tr.x) {
-          subjnewsubjp_v].x = clip.tr.x;
-        }
-      }
-      else if (c_v == 3) { // bottom
-        if (newsubj[p_v].y < clip.bl.y) {
-          newsubj[p_v].y = clip.bl.y;
-        }
-      }
+    //   else if (inside(S, edge)) {
+    //     P_x = computeIntersection(S, E, edge);
+    //     out.push_back(P_x);
+    //   }
+    //   S = E;
     }
-    newsubj
-    return subj;
   }
+  return out;
+}
+
+class NoClippingTest {
+public:
+  Polygon static get_polygon() {
+    Point P1 = {-0.5, -0.5};
+    Point P2 = { 0.5, -0.5};
+    Point P3 = { 0.5,  0.5};
+    Point P4 = {-0.5,  0.5};
+    Polygon P = {P1, P2, P3, P4};
+    return P;
+  }
+
+  Box static get_box() {
+    Point BL = {-1, -1};
+    Point TR = {1, 1};
+    Box B = {BL, TR};
+    return B;
+  }
+};
+
+void NoClipping() {
+  Polygon P = NoClippingTest::get_polygon();
+  std::cout << PolygonStr(P) << std::endl;
+  Box B = NoClippingTest::get_box();
+  std::cout << BoxStr(B) << std::endl;
+  Polygon P_clipped = ClipPolygon(P, B);
 }
 
 int main() {
+  NoClipping();
   return 0;
 }
